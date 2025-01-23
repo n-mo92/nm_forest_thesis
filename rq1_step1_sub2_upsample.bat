f946c09bb8ffab5d266473620f09743118882f32
Nina Moffat <ninamoffat@gmail.com>
2025-01-23 10:48:31 +0100

    update naming, add new .bat

--- rq1_step1_sub2_upsample.bat
+++ rq1_step1_sub2_upsample.bat
@@ -7,10 +7,10 @@
 cd thesis_env_conda\Library\bin
 
 :: Upsample ESA layer (300m) to 5m without interpolation
-::gdal_translate.exe -tr 5.0 5.0 -a_nodata 0.0 -ot Float32 -of GTiff -co COMPRESS=LZW -co BIGTIFF=YES %~dp0\processing\esa_lccs_class_3035_DE.tif %~dp0\processing\esa_lccs_class_3035_DE_5m.tif
+gdal_translate.exe -tr 5.0 5.0 -a_nodata 0.0 -ot Float32 -of GTiff -co COMPRESS=LZW -co BIGTIFF=YES %~dp0\processing\esa_lccs_class_3035_DE.tif %~dp0\processing\esa_lccs_class_3035_DE_5m.tif
 
 :: Upsample JAXA layer (25m) to 5m without interpolation
-gdal_translate.exe -tr 5.0 5.0 -a_nodata 0.0 -ot Float32 -of GTiff -co COMPRESS=LZW %~dp0\processing\jaxa_FNF_3035_DE.tif %~dp0\processing\jaxa_FNF_3035_DE_5m.tif
+::gdal_translate.exe -tr 5.0 5.0 -a_nodata 0.0 -ot Float32 -of GTiff -co COMPRESS=LZW %~dp0\processing\jaxa_FNF_3035_DE.tif %~dp0\processing\jaxa_FNF_3035_DE_5m.tif
 
 
 :: ADD HANSEN LATER
---
+++ rq1_step1_sub3_clip.bat
@@ -0,0 +1,4 @@
+:: WINDOWS BATCH FILE - UPSAMPLE
+
+:: When echo is turned off, the command prompt doesn't appear in the output
+@echo off
