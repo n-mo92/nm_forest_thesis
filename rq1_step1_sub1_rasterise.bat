c01a6a577d081febde0df4e5b9cf2d18fd20a940
Nina Moffat <ninamoffat@gmail.com>
2025-01-27 13:41:00 +0100

    update data type (UInt16)

--- rq1_step1_sub1_rasterise.bat
+++ rq1_step1_sub1_rasterise.bat
@@ -3,14 +3,14 @@
 :: When echo is turned off, the command prompt doesn't appear in the output
 @echo off
 
+:: Set up working folder (for inputs and ouputs)
+set "working_folder=%cd%\processing"
+
 :: Change directory to where gdal is stored
 cd thesis_env_conda\Library\bin
 
 :: Rasterise the CLC18 column for the GER LULC Class 3 data to 5m 
-gdal_rasterize.exe -l clc5_class3xx_3035_DE_int -a CLC18 -tr 5.0 5.0 -a_nodata 0.0 -ot Float32 -of GTiff -co COMPRESS=LZW %~dp0\processing\clc5_class3xx_3035_DE_int.shp %~dp0\processing\clc5_class3xx_3035_DE_5m.tif
+gdal_rasterize.exe -l clc5_class3xx_3035_DE_int -a CLC18 -tr 5.0 5.0 -a_nodata 0.0 -ot UInt16 -of GTiff -co COMPRESS=LZW %working_folder%\clc5_class3xx_3035_DE_int.shp %working_folder%\clc5_class3xx_3035_DE_5m.tif
 
 :: Rasterise the Code_18 column for the CORINE data to 5m
-gdal_rasterize.exe -l U2018_CLC2018_V2020_20u1 -a Code_18 -tr 5.0 5.0 -a_nodata 0.0 -ot Float32 -of GTiff -co COMPRESS=LZW %~dp0\processing\U2018_CLC2018_V2020_20u1.shp %~dp0processing\U2018_CLC2018_V2020_20u1_3035_DE_5m.tif
-
-
-:: Note: %~dp0 refers to the main directory in which this batch script is stored
+gdal_rasterize.exe -l U2018_CLC2018_V2020_DE -a Code_18 -tr 5.0 5.0 -a_nodata 0.0 -ot UInt16 -of GTiff -co COMPRESS=LZW %working_folder%\U2018_CLC2018_V2020_DE.shp %working_folder%\U2018_CLC2018_V2020_3035_DE_5m.tif
