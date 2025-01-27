75fddeb32e3de37333a9f034ad457ca3be521e8d
Nina Moffat <ninamoffat@gmail.com>
2025-01-27 13:46:18 +0100

    change to vrt output

--- rq1_step1_sub2_upsample.bat
+++ rq1_step1_sub2_upsample.bat
@@ -3,17 +3,17 @@
 :: When echo is turned off, the command prompt doesn't appear in the output
 @echo off
 
+:: Set up working folder (for inputs and ouputs)
+set "working_folder=%cd%\processing"
+
 :: Change directory to where gdal is stored
 cd thesis_env_conda\Library\bin
 
 :: Upsample ESA layer (300m) to 5m without interpolation
-gdal_translate.exe -tr 5.0 5.0 -a_nodata 0.0 -ot Float32 -of GTiff -co COMPRESS=LZW -co BIGTIFF=YES %~dp0\processing\esa_lccs_class_3035_DE.tif %~dp0\processing\esa_lccs_class_3035_DE_5m.tif
+gdal_translate.exe -tr 5.0 5.0 -a_nodata 0.0 -ot UInt16 %working_folder%\esa_lccs_class_3035_DE.tif %working_folder%\esa_lccs_class_3035_DE_5m.vrt
 
 :: Upsample JAXA layer (25m) to 5m without interpolation
-gdal_translate.exe -tr 5.0 5.0 -a_nodata 0.0 -ot Float32 -of GTiff -co COMPRESS=LZW %~dp0\processing\jaxa_FNF_3035_DE.tif %~dp0\processing\jaxa_FNF_3035_DE_5m.tif
+gdal_translate.exe -tr 5.0 5.0 -a_nodata 0.0 -ot UInt16 %working_folder%\jaxa_FNF_3035_DE.tif %working_folder%\jaxa_FNF_3035_DE_5m.vrt
 
 
 :: ADD HANSEN LATER
-
-
-:: Note: %~dp0 refers to the main directory in which this batch script is stored
