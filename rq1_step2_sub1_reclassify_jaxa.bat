b1e47181c65830481146ae33d54f33284ff4d19f
Nina Moffat <ninamoffat@gmail.com>
2025-01-28 14:27:41 +0100

    directory change

--- rq1_step2_sub1_reclassify_jaxa.bat
+++ rq1_step2_sub1_reclassify_jaxa.bat
@@ -4,11 +4,11 @@
 @echo off
 
 :: Set up input & output files
-set "input_file=%cd%\processing\jaxa_FNF_3035_DE.tif"
+set "input_file=%cd%\processing\jaxa_FNF_3035_DE_5m.vrt"
 set "ouput_file=%cd%\processing\jaxa_FNF_3035_DE_reclass.tif"
 
-:: Change directory to where gdal is stored
-cd thesis_env_conda\Library\bin
+:: Change directory to where gdal PYTHON is stored
+cd thesis_env_conda\Lib\site-packages\GDAL-3.10.1-py3.12-win-amd64.egg-info\scripts
 
 :: Reclassify JAXA (0 stays 0; 1 stays 1; 2 becomes 1; 3 or more becomes 0)
-gdal_calc.exe -A %input_file% --outfile=%output_file% --calc="0*(A==0)+1*(A==1)+1*(A==2)+0*(A>=3)" 
+gdal_calc.py -A %input_file% --outfile=%output_file% --calc="0*(A==0)+1*(A==1)+1*(A==2)+0*(A>=3)" 
