1a1873002ed19cd11d436d16795ec873edcb7f13
Nina Moffat <ninamoffat@gmail.com>
2025-01-29 10:31:00 +0100

    remove vrt creation

--- rq1_step1_sub1_rasterise.bat
+++ rq1_step1_sub1_rasterise.bat
@@ -15,6 +15,6 @@ gdal_rasterize.exe -l clc5_class3xx_3035_DE_int -a CLC18 -tr 5.0 5.0 -a_nodata 0
 :: Rasterise the Code_18 column for the CORINE data to 5m
 gdal_rasterize.exe -l U2018_CLC2018_V2020_DE -a Code_18 -tr 5.0 5.0 -a_nodata 0.0 -ot UInt16 -of GTiff -co COMPRESS=LZW %working_folder%\U2018_CLC2018_V2020_DE.shp %working_folder%\U2018_CLC2018_V2020_3035_DE_5m.tif
 
-:: Translate both tifs to vrts 
-gdal_translate.exe %working_folder%\clc5_class3xx_3035_DE_5m.tif %working_folder%\clc5_class3xx_3035_DE_5m.vrt
-gdal_translate.exe %working_folder%\U2018_CLC2018_V2020_3035_DE_5m.tif %working_folder%\U2018_CLC2018_V2020_3035_DE_5m.vrt
+:: Translate both tifs to vrts (removed for now - vrts generally seem to cause problems down the line)
+::gdal_translate.exe %working_folder%\clc5_class3xx_3035_DE_5m.tif %working_folder%\clc5_class3xx_3035_DE_5m.vrt
+::gdal_translate.exe %working_folder%\U2018_CLC2018_V2020_3035_DE_5m.tif %working_folder%\U2018_CLC2018_V2020_3035_DE_5m.vrt
