5dfdce5ec270e0a6dda72fc99e7f12947dc43991
Nina Moffat <ninamoffat@gmail.com>
2025-02-04 16:30:40 +0100

    add Hansen processing

--- rq1_step1_sub2_upsample.bat
+++ rq1_step1_sub2_upsample.bat
@@ -9,11 +9,11 @@ set "working_folder=%cd%\processing"
 :: Change directory to where gdal is stored
 cd thesis_env_conda\Library\bin
 
-:: Upsample ESA layer (300m) to 5m without interpolation
+:: Upsample ESA layer (300m) to 5m 
 gdal_translate.exe -tr 5.0 5.0 -a_nodata 0.0 -ot UInt16 -co COMPRESS=LZW -co BIGTIFF=YES %working_folder%\esa_lccs_class_3035_DE.tif %working_folder%\esa_lccs_class_3035_DE_5m.tif
 
-:: Upsample JAXA layer (25m) to 5m without interpolation
+:: Upsample JAXA layer (25m) to 5m 
 gdal_translate.exe -tr 5.0 5.0 -a_nodata 0.0 -ot UInt16 -co COMPRESS=LZW -co BIGTIFF=YES %working_folder%\jaxa_FNF_3035_DE.tif %working_folder%\jaxa_FNF_3035_DE_5m.tif
 
-
-:: ADD HANSEN LATER
+:: Update Hansen layer (30m) to 5m
+gdal_translate.exe -tr 5.0 5.0 -a_nodata -9999 -ot Int16 -co COMPRESS=LZW -co BIGTIFF=YES %working_folder%\hansen_60cover_2018_3035_DE.tif %working_folder%\hansen_60cover_2018_3035_DE_5m.tif
