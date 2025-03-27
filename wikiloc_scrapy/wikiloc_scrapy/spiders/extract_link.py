230002fa62af218224d25bedc81b09b21a8e12d5
Nina Moffat <ninamoffat@gmail.com>
2025-03-27 15:58:11 +0100

    extract link fix

--- wikiloc_scrapy/wikiloc_scrapy/spiders/extract_link.py
+++ wikiloc_scrapy/wikiloc_scrapy/spiders/extract_link.py
@@ -8,48 +8,26 @@ class WikilocSpiderSpider(scrapy.Spider):
     name = 'wiki'
     allowed_domains = ['wikiloc.com']
     start_urls = ['https://www.wikiloc.com/trails/outdoor/germany/bremen']   #your link
-    liste = []
+    #liste = [] # NM: not used?
     def parse(self, response):
-        villes = response.xpath('//div[@id="filters"]/ul/li/a/@href').extract()
+        # NM: for ul added [1] to only get city filters (and not activity type filters)
+        villes = response.xpath('//div[@id="filters"]/ul[1]/li/a/@href').extract()
         for v in villes :
             yield Request(v, self.parse_detail)
     def parse_detail(self, response):
+        #NM: hard-code base URL
+        base = 'https://www.wikiloc.com/trails/outdoor/germany/bremen' #your link
         trails = response.xpath('//ul[@class="trail-list"]/li')
-        next = response.xpath('//a[@class="next"]/@href').extract_first()
+        # NM: changed xpath from '//a[@class="next"]/@href'
+        next = response.xpath('//a[@rel="next"]/@href').extract_first()
         for t in trails:
-            link = t.xpath('./div/div/h3/a/@href').extract_first()
+            # NM: changed xpath from './div/div/h3/a/@href'
+            link = t.xpath('./article/div[1]/h2/a/@href').extract_first()
             if link is not None:
                 item={}
                 item["Link"] = link
                 yield item
         if next is not None:
-            base = response.url.split('?')[0]
+            # NM: adjust to use hard-coded base URL 
             next_page = base + next
             yield Request(next_page, self.parse_detail)
-
-
-# Old version
-"""
-class WikilocSpiderSpider(scrapy.Spider):
-    name = 'wiki'
-    allowed_domains = ['wikiloc.com']
-    start_urls = ['https://www.wikiloc.com/trails/outdoor/germany/niedersachsen']   #your link
-    liste = []
-    def parse(self, response):
-        villes = response.xpath('//div[@id="filters"]/ul/li/a/@href').extract()
-        for v in villes :
-            yield Request(v, self.parse_detail)
-    def parse_detail(self, response):
-        trails = response.xpath('//ul[@class="trail-list"]/li')
-        next = response.xpath('//a[@class="next"]/@href').extract_first()
-        for t in trails:
-            link = t.xpath('./div/div/h3/a/@href').extract_first()
-            if link is not None:
-                item={}
-                item["Link"] = link
-                yield item
-        if next is not None:
-            base = response.url.split('?')[0]
-            next_page = base + next
-            yield Request(next_page, self.parse_detail)
-"""
