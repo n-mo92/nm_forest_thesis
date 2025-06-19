546166763c8ff991e0c331fea07b5f5e032f3034
Nina Moffat <ninamoffat@gmail.com>
2025-06-19 18:53:54 +0200

    update xpaths

--- wikiloc_scrapy/wikiloc_scrapy/spiders/extract_link_large.py
+++ wikiloc_scrapy/wikiloc_scrapy/spiders/extract_link_large.py
@@ -5,7 +5,7 @@ from scrapy.http import Request
 class WikilocSpiderSpider(scrapy.Spider):
     name = 'wiki_large'
     allowed_domains = ['wikiloc.com']
-    start_urls = ['https://www.wikiloc.com/directory/bjnYAg']   # link to place list WITHIN region
+    start_urls = ['https://www.wikiloc.com/directory/JuqRut']   # link to place list WITHIN region
     # Loop through the towns in the region
     def parse(self, response):
         towns = response.xpath('//div[@class="directory__item__list__item"]/a/@href').extract() #updated xpath 06/06/2025
@@ -22,14 +22,24 @@ class WikilocSpiderSpider(scrapy.Spider):
                 a_url = response.urljoin(a)
                 yield Request(a_url, self.parse_activity_trails)
         else:
-            trails = response.xpath('//ul[@class="geo-landing__body__canonical-trails__list"]/div[@class="figure__card__panel"]') #updated xpath 17/06/2025 
-            next = response.xpath('//a[@rel="next"]/@href').extract_first() #updated xpath 17/06/2025      
+            # Formatting for when there only a few trails
+            trails = response.xpath('//ul[@class="geo-landing__body__canonical-trails__list"]/li[@class="geo-landing__body__canonical-trails__list__items geo-landing__body__canonical-trails__list__item--other"]') #updated xpath 17/06/2025 
             for t in trails:
-                link = t.xpath('//a/@href').extract_first() #updated xpath 17/06/2025
+                link = t.xpath('./article/@data-prettyurl').extract_first() #updated xpath 17/06/2025
                 if link is not None:
                     item={}
                     item["Link"] = response.urljoin(link) #edited from link 
                     yield item
+            # Formatting for when there many trails
+            trails_more = response.xpath('//ul[@class="landing__body__trails__list"]/li') #updated xpath 17/06/2025 
+            for t in trails_more:
+                link = t.xpath('./article/div[1]/h3/a/@href').extract_first() #updated xpath 17/06/2025
+                if link is not None:
+                    item={}
+                    item["Link"] = response.urljoin(link) #edited from link 
+                    yield item
+            # Pagination
+            next = response.xpath('//a[@rel="next"]/@href').extract_first() #updated xpath 17/06/2025
             if next is not None:
                 #base = response.url.split('?')[0] 
                 next_page = response.urljoin(next) #edited from base + next
